13d47c7fa18317690160e6d18df715371902294b
Jamie Keeling <jakeeling5@gmail.com>
2026-05-17 20:55:28 +0100

    Adding v0.2.1 release

--- Formula/tide.rb
+++ Formula/tide.rb
@@ -4,10 +4,9 @@
 class Tide < Formula
   desc "Terminal IDE written in Rust"
   homepage "https://github.com/jamiek2142/tide"
-  url "https://github.com/jamiek2142/tide/releases/download/0.2.0/tide-mac.tar.gz"
-  sha256 "c3cbc9f0c12b7857ac249ffe97a0e4bbf7071e443e64b6aabbdaa4f701aee36e"
-  version "0.2.0"
-
+  url "https://github.com/jamiek2142/tide/releases/download/0.2.1/tide-mac.tar.gz"
+  sha256 "e68f3d1825fe5a852e61fa848eec7e01d8ecc4b75b2229d0a1cb22223c75c237"
+  version "0.2.1"
   def install
     bin.install "tide"
   end
